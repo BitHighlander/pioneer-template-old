e1c2d706c1bcbf6ae5664e0ebbaab941e1af6ed8
bithighlander <bithighlander@gmail.com>
2023-08-08 12:13:35 -0500

    context example

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CTASection from "./components/CTASection";
 import SomeImage from "./components/SomeImage";
 import SomeText from "./components/SomeText";
+import { usePioneer } from "pioneer-react";
 
 const Home = () => {
+  const { state } = usePioneer();
+  const { api, app, context, assetContext, blockchainContext, pubkeyContext } = state;
+  const [address, setAddress] = useState("");
+
+  useEffect(() => {
+    console.log("pubkeyContext: ", pubkeyContext);
+    setAddress(pubkeyContext.master || pubkeyContext.pubkey);
+  }, [pubkeyContext]);
+
   return (
     <div>
+      Context: {context}
+      <br />
+      Asset Context: {assetContext?.name}
+      <br />
+      Blockchain Context: {blockchainContext?.name}
+      <br />
+      Address: {pubkeyContext?.master || pubkeyContext?.pubkey}
+      <br />
       <SomeText />
       <SomeImage />
       <CTASection />
